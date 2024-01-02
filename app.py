7586a9bff38242c60cc7d9253d878e158046f828
rahulmuggalla28 <93699515+rahulmuggalla28@users.noreply.github.com>
2024-01-02 13:59:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,16 @@
 
 from flask import Flask, render_template, request
 import numpy as np
-import joblib
-import os
+import pickle
+
 app = Flask(__name__)
 
 # Load the pre-trained model
-path=os.path.dirname(os.path.abspath(__file__))
-model = joblib.load(os.path.join(path,'house_prediction_model.pkl'))
+with open('Model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+with open('Scaler.pkl', 'rb') as f:
+    scaler = pickle.load(f)
 
 # Define the route for the home page
 @app.route('/')
@@ -30,8 +33,10 @@ def predict():
     # Create input data array
     input_data = np.array([[bed, bath, loc, size, status, face, Type]])
 
+    input_df = scaler.transform(input_data)
+
     # Predict the price using the pre-trained model
-    predicted_price = model.predict(input_data)[0]
+    predicted_price = model.predict(input_df)[0]
 
     # Render the result page with predicted price
     return render_template('index.html', predicted_price=predicted_price)
